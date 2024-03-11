2b2447aea36fac5a448ad3ccbecf97d6baec1985
dionb <dionb@Mogira>
2024-03-11 18:17:00 -0500

    initialized cookingStrategy to null in default constructor.

--- src/edu/mu/pizzaOrder/AbstractPizza.java
+++ src/edu/mu/pizzaOrder/AbstractPizza.java
@@ -15,6 +15,7 @@ public abstract class AbstractPizza {
     public AbstractPizza() {
         toppingList = new ArrayList<>();
         // Initialize other attributes here
+        cookingStrategy = null;
     }
     
     public AbstractPizza(AbstractPizza Pizza) {
