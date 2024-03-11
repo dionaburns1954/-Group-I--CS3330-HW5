a239542d678d3e37ef3918ee8f99097ac3288290
dionb <dionb@Mogira>
2024-03-11 15:13:09 -0500

    added implementation to createPizza method

--- src/edu/mu/pizzaOrder/PizzaCookingFactory.java
+++ src/edu/mu/pizzaOrder/PizzaCookingFactory.java
@@ -1,5 +1,38 @@
 package edu.mu.pizzaOrder;
 
 public class PizzaCookingFactory {
-
+	
+	public AbstractPizza createPizza(PizzaType pizzaType) {
+		AbstractPizza p;
+		if(pizzaType == PizzaType.HAWAIIAN) { //creates Hawaiian pizza
+    		p = new HawaiianPizza();
+    		
+ 
+    		return p;
+    	}
+    	else if(pizzaType == PizzaType.MARGHERITA) { //creates Margherita pizza
+    		p = new MargheritaPizza();
+    		
+    		
+ 
+    		return p;
+    	}
+    	else if(pizzaType == PizzaType.SUPREME) { //creates supreme pizza
+    		p = new SupremePizza();
+    		
+    		
+    		return p;
+    	}
+    	else if(pizzaType == PizzaType.VEGETARIAN){ //creates vegetarian pizza
+    		p = new VegetarianPizza();
+    		
+    		
+    		
+    		return p;
+    	}
+    	
+		
+		
+		return null; // returns false if the topping given is non of the ones registered
+	}
 }
