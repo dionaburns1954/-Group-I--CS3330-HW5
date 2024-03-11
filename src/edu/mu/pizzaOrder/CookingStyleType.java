02e7eb8674c3376282203ed6ccf30597e7b5742f
RedPh <lmb7gq@umsystem.edu>
2024-03-10 22:51:44 -0500

    fixed up cooking style enum to add extra cooking price and getter and setters for that AS well as create Pizza type enum with getter and setters for price

--- src/edu/mu/pizzaOrder/CookingStyleType.java
+++ src/edu/mu/pizzaOrder/CookingStyleType.java
@@ -1,7 +1,18 @@
 package edu.mu.pizzaOrder;
 
 public enum CookingStyleType {
-	MICROWAVE,
-	CONVENTIONAL_OVEN,
-	BRICK_OVEN
+	// left hand side is cooking method right hand side is price of cooking method
+	MICROWAVE(10.00),
+	CONVENTIONAL_OVEN(8.00),
+	BRICK_OVEN(1.00);
+	
+	private double additionalCookingPrice;// set additional price 
+	
+	CookingStyleType(double additionalCookingPrice){
+		this.additionalCookingPrice = additionalCookingPrice;
+	}
+	// getter for price 
+	public double getAdditionalCookingPrice() {
+		return additionalCookingPrice;
+	}
 }
--- src/edu/mu/pizzaOrder/PizzaType.java
+++ src/edu/mu/pizzaOrder/PizzaType.java
@@ -1,5 +1,19 @@
 package edu.mu.pizzaOrder;
 
 public enum PizzaType {
-
+	// set enum up with left hand side being pizza type and right hand side being initial price of pizza 
+	HAWAIIAN(1.50),
+	MARGHERITA(2.00),
+	SUPREME(3.50),
+	VEGETARIAN(1.50);
+	
+	private double toppingPrice;// set price of enum 
+	
+	PizzaType(double toppingPrice){//have setter 
+		this.toppingPrice = toppingPrice;
+	}
+	// have a getter for price of pizza 
+	public double getToppingPrice()
+{
+		return toppingPrice;}
 }
