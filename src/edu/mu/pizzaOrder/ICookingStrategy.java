df59ccee3f527507eb7c6445c62cef7c0cf5b577
azfcf <azfcf@umsystem.edu>
2024-03-12 12:25:49 -0500

    Add javadoc comments to ICookingstrategy

--- src/edu/mu/pizzaOrder/ICookingStrategy.java
+++ src/edu/mu/pizzaOrder/ICookingStrategy.java
@@ -1,5 +1,21 @@
 package edu.mu.pizzaOrder;
 
+/**
+ * Generic interface for cooking strategies
+ * @author Ryan Esparza
+ * @version 1.0
+ * 
+ * @see BrickOvenCookingStrategy
+ * @see ConventionalOvenCookingStrategy
+ * @see MicrowaveCookingStrategy
+ */
 public interface ICookingStrategy {
+	
+	/**
+	 * Changes {@link AbstractPizza#cookingPrice} and 
+	 * {@link AbstractPizza#cookingStrategy} to the relevant cooking strategy.
+	 * @param pizza the pizza to apply the cooking method to
+	 * @return the result of the operation
+	 */
 	public boolean cook(AbstractPizza pizza);
 }
